7c6b0cb8a4eedca93bb85338366c119c4f38db4a
杨华 <593283083@qq.com>
2015-07-01 14:27:12 +0800

    Initial Commit

---
+++ 首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  首页
+//
+//  Created by xxzx on 15/7/1.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 首页/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  首页
+//
+//  Created by xxzx on 15/7/1.
+//  Copyright (c) 2015年 hxyxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
